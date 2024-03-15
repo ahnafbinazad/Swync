40a43070eb2e11c773a6978dfa1f22a72b77741c
Ahnaf Azad <ahnaf2232@gmail.com>
2024-03-15 09:43:54 +0000

    updated user model

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,19 +1,49 @@
 class UserModel {
-  final String? id;
-  final String? userName;
-  final String? email;
+  final String userId;
+  final String email;
+  final String username;
+  final int totalWorkoutTime;
+  final int totalWorkoutDays;
+  final int bestStreak;
+  final Map<String, dynamic> bestRank;
+  final List<dynamic> friends;
+  final int streak;
+  final int monthlyWorkoutTime;
+  final int streakRank;
+  final int workoutRank;
+  final int league;
 
-  const UserModel ({
-  this.id,
-  required this.email,
-  required this.userName
-});
+  UserModel({
+    required this.userId,
+    required this.email,
+    required this.username,
+    required this.totalWorkoutTime,
+    required this.totalWorkoutDays,
+    required this.bestStreak,
+    required this.bestRank,
+    required this.friends,
+    required this.streak,
+    required this.monthlyWorkoutTime,
+    required this.streakRank,
+    required this.workoutRank,
+    required this.league,
+  });
 
-toJson() {
-  return {
-    "userName": userName,
-    "email": email,
-  };
+  factory UserModel.fromFirestore(Map<String, dynamic> data) {
+    return UserModel(
+      userId: data['userId'],
+      email: data['email'],
+      username: data['username'],
+      totalWorkoutTime: data['totalWorkoutTime'],
+      totalWorkoutDays: data['totalWorkoutDays'],
+      bestStreak: data['bestStreak'],
+      bestRank: data['bestRank'],
+      friends: List<dynamic>.from(data['friends']),
+      streak: data['streak'],
+      monthlyWorkoutTime: data['monthlyWorkoutTime'],
+      streakRank: data['streakRank'],
+      workoutRank: data['workoutRank'],
+      league: data['league'],
+    );
+  }
 }
-}
-
