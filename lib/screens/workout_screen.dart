eb666b619760ab74cfa31cebc2fb00287d875e8e
Ahnaf Azad <ahnaf2232@gmail.com>
2024-03-21 23:23:19 +0000

    popup for workout complete

--- lib/screens/workout_screen.dart
+++ lib/screens/workout_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:test_drive/database/db_service.dart';
+import 'package:test_drive/screens/home.dart';
 
 class RecordWorkoutScreen extends StatefulWidget {
   @override
@@ -8,66 +12,141 @@ class RecordWorkoutScreen extends StatefulWidget {
 
 class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
   late Future<CameraController> _cameraControllerFuture;
-  
+  late CameraController _cameraController;
+  bool _isRecording = false;
+  Stopwatch _stopwatch = Stopwatch();
+  late Timer _timer;
+  int _elapsedMinutes = 0;
+
   @override
   void initState() {
     super.initState();
-    _cameraControllerFuture = _initializeCamera();
-      }
+    _initializeCamera();
+  }
 
-  Future<CameraController> _initializeCamera() async {
+  Future<void> _initializeCamera() async {
     try {
       final cameras = await availableCameras();
       if (cameras.isEmpty) {
         throw Exception('No cameras available');
       }
       final firstCamera = cameras.first;
-      final controller = CameraController(firstCamera, ResolutionPreset.high);
-      await controller.initialize();
-      return controller;
+      _cameraController = CameraController(firstCamera, ResolutionPreset.high);
+      await _cameraController.initialize();
+      setState(() {});
     } catch (e) {
       // Handle error appropriately
       print('Error initializing camera: $e');
-      rethrow; // Rethrow the exception to propagate it to the caller
     }
+  }
+
+  void _startRecording() {
+    setState(() {
+      _isRecording = true;
+      _stopwatch.reset();
+      _stopwatch.start();
+      _timer = Timer.periodic(Duration(seconds: 60), (timer) {
+        setState(() {
+          _elapsedMinutes = _stopwatch.elapsed.inMinutes;
+        });
+      });
+    });
+    _cameraController.startVideoRecording();
+  }
+
+  void _stopRecording() async {
+    setState(() {
+      _isRecording = false;
+      _stopwatch.stop();
+      _timer.cancel();
+      _elapsedMinutes = _stopwatch.elapsed.inMinutes;
+    });
+
+    // Show a dialog with the recorded workout time
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Workout Duration'),
+          content: Text('You recorded a workout for $_elapsedMinutes minutes.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+
+
+                // Navigate to HomeScreen
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                );
+              },
+              child: Text('Okay'),
+            ),
+          ],
+        );
+      },
+    );
+
+    await _cameraController.stopVideoRecording();
   }
 
   @override
   void dispose() {
-    _cameraControllerFuture.then((controller) {
-      controller.dispose();
-    });
+    _cameraController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_cameraController == null || !_cameraController.value.isInitialized) {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('Record Workout'),
       ),
-      body: FutureBuilder<CameraController>(
-        future: _cameraControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            } else if (snapshot.data != null) {
-              final controller = snapshot.data!;
-              return CameraPreview(controller);
-            } else {
-              return Center(
-                child: Text('Unable to initialize camera.'),
-              );
-            }
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                CameraPreview(_cameraController),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      'Recording Time: $_elapsedMinutes minutes',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: _isRecording ? null : _startRecording,
+                child: Icon(Icons.video_call),
+              ),
+              SizedBox(width: 20.0),
+              ElevatedButton(
+                onPressed: _isRecording ? _stopRecording : null,
+                child: Icon(Icons.stop),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
