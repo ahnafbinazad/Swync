16fa3caf5a415fd441ebf2caa386935e094cc3cf
Ahnaf Azad <ahnaf2232@gmail.com>
2024-04-17 12:58:13 +0100

    refinements

--- lib/screens/workout_screen.dart
+++ lib/screens/workout_screen.dart
@@ -1,33 +1,31 @@
-import 'dart:async'; // Import statement for Timer
+import 'dart:async';
+import 'package:camera/camera.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
 import 'package:test_drive/database/db_service.dart';
 import 'package:test_drive/screens/home.dart';
- 
+
 class RecordWorkoutScreen extends StatefulWidget {
   @override
   _RecordWorkoutScreenState createState() => _RecordWorkoutScreenState();
 }
- 
+
 class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
-  final DbService _dbService = DbService(FirebaseFirestore.instance,
-      FirebaseAuth.instance); // Declare and initialize DbService instance
-  CameraController? _cameraController; // Initialize as null
+  final DbService _dbService = DbService(FirebaseFirestore.instance, FirebaseAuth.instance);
+  CameraController? _cameraController;
   bool _isRecording = false;
   Stopwatch _stopwatch = Stopwatch();
   late Timer _timer;
   String _workoutDuration = '00:00:00';
- 
+
   @override
   void initState() {
     super.initState();
-    // Initialize the camera controller
     _initializeCamera();
     _dbService.syncAppUser();
   }
- 
+
   Future<void> _initializeCamera() async {
     try {
       final cameras = await availableCameras();
@@ -39,11 +37,18 @@ class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
       await _cameraController!.initialize();
       setState(() {});
     } catch (e) {
-      // Handle error appropriately
       print('Error initializing camera: $e');
     }
   }
- 
+
+  void _toggleRecording() {
+    if (_isRecording) {
+      _stopRecording();
+    } else {
+      _startRecording();
+    }
+  }
+
   void _startRecording() {
     setState(() {
       _isRecording = true;
@@ -57,26 +62,26 @@ class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
     });
     _cameraController!.startVideoRecording();
   }
- 
+
   void _stopRecording() async {
     setState(() {
       _isRecording = false;
       _stopwatch.stop();
       _timer.cancel();
-      _updateDuration(); // Update UI with precise duration
+      _updateDuration();
     });
- 
+
     // Show "analysing workout" popup
     showDialog(
       context: context,
-      barrierDismissible: false, // Prevent user from dismissing the popup
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('Analysing Workout'),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              CircularProgressIndicator(), // Circular motion indicator
+              CircularProgressIndicator(),
               SizedBox(height: 10),
               Text('Please wait...'),
             ],
@@ -84,19 +89,15 @@ class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
         );
       },
     );
- 
-    // Delay for 3 seconds
+
     await Future.delayed(Duration(seconds: 3));
- 
-    // Close the "analysing workout" popup
+
     Navigator.pop(context);
- 
+
     final duration = _stopwatch.elapsed;
     final totalSeconds = duration.inSeconds;
-    final minutes = totalSeconds ~/
-        60; // Use integer division to round down to lower minute
- 
-    // Show a dialog with the recorded workout time
+    final minutes = totalSeconds ~/ 60;
+
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -108,8 +109,7 @@ class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
             actions: <Widget>[
               TextButton(
                 onPressed: () {
-                  Navigator.pop(context); // Close the dialog
-                  // Navigate to HomeScreen
+                  Navigator.pop(context);
                   Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(builder: (context) => HomeScreen()),
@@ -120,9 +120,7 @@ class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
             ],
           );
         } else {
-          // Call addStreak function if workout duration is over 2 minutes
           _dbService.addStreak();
-          // Update workout time if workout duration is over 2 minutes
           _dbService.updateWorkoutTime(minutes);
           return AlertDialog(
             title: Text('Workout Duration'),
@@ -130,7 +128,6 @@ class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
             actions: <Widget>[
               TextButton(
                 onPressed: () {
-                  // Navigate to HomeScreen
                   Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(builder: (context) => HomeScreen()),
@@ -143,10 +140,10 @@ class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
         }
       },
     );
- 
+
     await _cameraController!.stopVideoRecording();
   }
- 
+
   void _updateDuration() {
     final duration = _stopwatch.elapsed;
     final minutes = duration.inMinutes.remainder(60).toString().padLeft(2, '0');
@@ -158,13 +155,13 @@ class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
       _workoutDuration = '$minutes:$seconds:$milliseconds';
     });
   }
- 
+
   @override
   void dispose() {
     _cameraController?.dispose();
     super.dispose();
   }
- 
+
   @override
   Widget build(BuildContext context) {
     if (_cameraController == null || !_cameraController!.value.isInitialized) {
@@ -174,27 +171,29 @@ class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
         ),
       );
     }
-    // Once _cameraController is initialized, build the UI
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Record Workout'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Expanded(
             child: Stack(
+              alignment: Alignment.center,
               children: [
-                CameraPreview(_cameraController!),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text(
-                      'Workout Duration: $_workoutDuration',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                      ),
+                Positioned.fill(
+                  child: CameraPreview(_cameraController!),
+                ),
+                Positioned(
+                  bottom: 20.0,
+                  child: Text(
+                    'Workout Duration: $_workoutDuration',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
                     ),
                   ),
                 ),
@@ -202,26 +201,23 @@ class _RecordWorkoutScreenState extends State<RecordWorkoutScreen> {
             ),
           ),
           SizedBox(height: 20.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: _isRecording ? null : _startRecording,
-                child: Icon(Icons.video_call),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: ElevatedButton.icon(
+              onPressed: _toggleRecording,
+              icon: Icon(_isRecording ? Icons.stop : Icons.video_call),
+              label: Text(_isRecording ? 'Stop Recording' : 'Start Recording'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               ),
-              SizedBox(width: 20.0),
-              ElevatedButton(
-                onPressed: _isRecording ? _stopRecording : null,
-                child: Icon(Icons.stop),
-              ),
-            ],
+            ),
           ),
         ],
       ),
     );
   }
 }
- 
+
 void main() {
   runApp(MaterialApp(
     home: RecordWorkoutScreen(),
